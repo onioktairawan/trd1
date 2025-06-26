33b678a7cd016ee28bd9353c86116b36f785afa1
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-06-26 14:39:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 MONGO_URI = "mongodb+srv://idopop3:Irawanoni@oni.a8z7e6x.mongodb.net/?retryWrites=true&w=majority&appName=oni"
-DB_NAME = "jurnal_trading2"
-COLLECTION_NAME = "trades"
+DB_NAME = "trading_journal4"
+COLLECTION_NAME = "tradess"  # atau 'jurnal' jika kamu pakai nama itu
+USERS_COLLECTION_NAME = "users"
